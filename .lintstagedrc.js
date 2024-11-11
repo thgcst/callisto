03aa7602f2cc35111c108bdec65ed0663708c679
Thiago Costa <44840709+thgcst@users.noreply.github.com>
2024-11-11 00:02:44 -0300

    Costa/implement pre commit (#8)
    * feat: husky
    
    * feat: lint-staged
    
    * feat: prettier
    
    * feat: add github actions
    
    * feat: parallelize
    
    * Revert "feat: parallelize"
    
    This reverts commit 38b961ee67f23e93aef7217d63c3bd1ad3aeb5b3.

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+module.exports = {
+  "*.{ts,tsx}": [buildEslintCommand, "bash -c 'yarn check-types'"],
+  "*": ["prettier --write"],
+};
