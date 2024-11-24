52843c2dba7d46cdf61131ce862b62f7b9aacb85
Thiago <thiagodias2708@gmail.com>
2024-11-24 01:18:33 -0300

    fix: keep only one migration

--- src/infra/prisma/migrations/20241124041810_create_company_table/migration.sql
+++ src/infra/prisma/migrations/20241124041810_create_company_table/migration.sql
@@ -16,20 +16,17 @@ CREATE TABLE "companies" (
     "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMPTZ NOT NULL,
 
-    CONSTRAINT "companies_pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "formalized_check" CHECK (
-        NOT "formalized" OR (
-            "cnpj" IS NOT NULL AND
-            "email" IS NOT NULL AND
-            "phoneNumber" IS NOT NULL
-        )
-    )
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "_CompanyPartners" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL
+CREATE TABLE "company_partners" (
+    "companyId" TEXT NOT NULL,
+    "individualId" TEXT NOT NULL,
+    "assignedAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "assignedById" TEXT NOT NULL,
+
+    CONSTRAINT "company_partners_pkey" PRIMARY KEY ("companyId","individualId")
 );
 
 -- CreateIndex
@@ -41,12 +38,6 @@ CREATE UNIQUE INDEX "companies_email_key" ON "companies"("email");
 -- CreateIndex
 CREATE UNIQUE INDEX "companies_addressId_key" ON "companies"("addressId");
 
--- CreateIndex
-CREATE UNIQUE INDEX "_CompanyPartners_AB_unique" ON "_CompanyPartners"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_CompanyPartners_B_index" ON "_CompanyPartners"("B");
-
 -- AddForeignKey
 ALTER TABLE "individuals" ADD CONSTRAINT "individuals_companyAsEmployeeId_fkey" FOREIGN KEY ("companyAsEmployeeId") REFERENCES "companies"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
@@ -57,7 +48,10 @@ ALTER TABLE "companies" ADD CONSTRAINT "companies_addressId_fkey" FOREIGN KEY ("
 ALTER TABLE "companies" ADD CONSTRAINT "companies_approvedByUserId_fkey" FOREIGN KEY ("approvedByUserId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_CompanyPartners" ADD CONSTRAINT "_CompanyPartners_A_fkey" FOREIGN KEY ("A") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "company_partners" ADD CONSTRAINT "company_partners_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_CompanyPartners" ADD CONSTRAINT "_CompanyPartners_B_fkey" FOREIGN KEY ("B") REFERENCES "individuals"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "company_partners" ADD CONSTRAINT "company_partners_individualId_fkey" FOREIGN KEY ("individualId") REFERENCES "individuals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "company_partners" ADD CONSTRAINT "company_partners_assignedById_fkey" FOREIGN KEY ("assignedById") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
